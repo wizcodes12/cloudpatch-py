29216c5f71dddf06379f50791b636257be6da991
WizCodes <137535009+wizcodes12@users.noreply.github.com>
2025-03-20 00:31:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3786,7 +3786,9 @@ async def send_fix(request: FixRequest, session: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 # venv\Scripts\activate
 # uvicorn main:app --reload
